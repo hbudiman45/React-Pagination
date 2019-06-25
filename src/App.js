9b0c8aa6835a919df96b874b1cc9e95a1e006f9d
Haekal Budiman <hbudiman45@gmail.com>
2019-06-25 16:59:01 +0700

    simple paginate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Post from "./components/Post";
 import "./App.css";
+import Pagination from "./components/Pagination";
 
 const App = () => {
-  const []
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setPosts(res.data);
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
+  // Get Current Posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  // Paginate Function
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
   return (
     <div className="container mt-5">
-      <h1>My Blog</h1>
+      <h1 className="text-primary mb-3">My Blog</h1>
+      <Post posts={currentPosts} loading={loading} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
     </div>
   );
 };
